c4ddec71d48f45a126fe786b173ac54cc69e1809
Benjamin SICARD <ext-benjamin.sicard@numias.com>
2024-08-14 10:36:28 +0200

    refactor: use the medals_order_by component in sql query intead of conditional display

--- evidence/queries/medals_total_paris2024_ordered_by_gold.sql
+++
@@ -1,7 +0,0 @@
-with source as (
-
-    select * from duckdb_paris2024.medals_total_paris2024 where country is not null
-
-)
-
-from source order by gold_medal_count desc, silver_medal_count desc, bronze_medal_count desc
